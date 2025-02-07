46b814d3a991577e08494cb4c1632521dded19f7
tres <59184373+pybash@users.noreply.github.com>
2025-02-07 18:55:55 -0500

    Layout changes. Doing branches now

--- sse-display-next/src/app/page.tsx
+++ sse-display-next/src/app/page.tsx
@@ -1,8 +1,15 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
+import StudentMentor from "./slides/StudentMentor";
 
 export default function Home() {
+  const slides = [<StudentMentor />]
+  const [onSlide, setSlide] = useState(0);
+
   return (
-    <div>
+    <div className="flex w-[95%] h-[90%]">
+      {slides[onSlide]}
     </div>
   );
 }
---
+++ sse-display-next/src/app/slides/StudentMentor.tsx
@@ -0,0 +1,13 @@
+
+export default function StudentMentor () {
+    return(
+        <div className="w-full h-full flex flex-col">
+            <div className="w-full h-[20%] flex">
+
+            </div>
+            <div className="w-full h-[80%]">
+
+            </div>
+        </div>
+    )
+}
